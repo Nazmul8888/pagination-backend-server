4936f0fb93edf1bd395274b61c68ac991854e43e
Nazmul8888 <nazmulkhan416@gmail.com>
2023-11-02 14:54:12 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,8 +12,10 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6nodxbc.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,8 +34,32 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log('pagination query', page,size);
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
         res.send(result);
+    })
+
+    app.post('/productByIds',async(req,res)=>{
+      const ids = req.body;
+      const idWithObjectId = ids.map(id => new ObjectId(id))
+      const query = {
+        _id:{
+          $in: idWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/productsCount', async(req,res)=>{
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({count})
+      
     })
 
     // Send a ping to confirm a successful connection
